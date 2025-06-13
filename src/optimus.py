553931803d5bf6c8117ea21d141d36800d9f8296
Bruno Arsioli <57162790+BrunoArsioli@users.noreply.github.com>
2025-06-13 11:30:28 +0100

    added new target metrics and arg: tg, pp, mean

--- src/optimus.py
+++ src/optimus.py
@@ -5,7 +5,6 @@ import os
 import shutil
 import pandas as pd 
 import tempfile
-import subprocess 
 import argparse 
 
 # count number of available cpus
@@ -57,54 +56,72 @@ def estimate_max_ngl(min_ngl=0, max_ngl=SEARCH_SPACE['gpu_layers']['high']):
 
 
 
-def run_llama_bench_with_csv(cmd):
+def run_llama_bench_with_csv(cmd, metric):
     # cmd should include: ... "-o", "csv"
+    # use metric (a string: "tg", "pp", or "mean")
+
     result = subprocess.run(cmd, capture_output=True, text=True, timeout=180)
+    if result.returncode != 0:
+        raise RuntimeError(result.stderr)
     
     # for debug 
     print(result.stdout)
-
-    if result.returncode != 0:
-        raise RuntimeError(result.stderr)
 
     # Save stdout to a temp CSV file
     with tempfile.NamedTemporaryFile(suffix=".csv", delete=False, mode="w") as csvfile:
         csvfile.write(result.stdout)
         csv_path = csvfile.name
 
-    df = pd.read_csv(csv_path)
-    tg_rows = df[df["n_gen"] > 0]  # catch the rows with results for the token generation speed 
-    if tg_rows.empty:
-        raise ValueError("No 'tg' (throughput) test results found in output CSV")
-    return float(tg_rows["avg_ts"].iloc[0])
+        df = pd.read_csv(csv_path)
+        metric_value = 0. # start metric value
+
+        if metric == "tg":
+            tg_rows = df[df["n_gen"] > 0]
+            if not tg_rows.empty: # writes only if tg_row is not empty 
+                metric_value = float(tg_rows["avg_ts"].iloc[0])
+        elif metric == "pp":
+            pp_rows = df[df["n_prompt"] > 0]
+            if not pp_rows.empty: # writes only if pp_row is not empty 
+                metric_value = float(pp_rows["avg_ts"].iloc[0])
+        elif metric == "mean":
+            tg_rows = df[df["n_gen"] > 0]
+            pp_rows = df[df["n_prompt"] > 0]
+            if not tg_rows.empty and not pp_rows.empty: # writes only if tg_ and pp_row are not empty 
+                metric_value = 0.5 * (float(tg_rows["avg_ts"].iloc[0]) + float(pp_rows["avg_ts"].iloc[0]))
+        return metric_value
 
 
 def objective(trial):
     # Sample params
-    threads    = trial.suggest_int('threads', SEARCH_SPACE['threads']['low'], SEARCH_SPACE['threads']['high'])
-    batch = trial.suggest_categorical('batch', SEARCH_SPACE['batch_size'])
-    u_batch = trial.suggest_categorical('u_batch', SEARCH_SPACE['ubatch_size'])
-    gpu_layers = trial.suggest_int('gpu_layers', SEARCH_SPACE['gpu_layers']['low'], SEARCH_SPACE['gpu_layers']['high'])
+    batch      = trial.suggest_categorical('batch', SEARCH_SPACE['batch_size'])
     flash      = trial.suggest_categorical('flash', SEARCH_SPACE['flash_attn'])
+    u_batch    = trial.suggest_categorical('u_batch', SEARCH_SPACE['ubatch_size'])
+    threads    = trial.suggest_int('threads', SEARCH_SPACE['threads']['low'], SEARCH_SPACE['threads']['high'])
+    gpu_layers = trial.suggest_int('gpu_layers', SEARCH_SPACE['gpu_layers']['low'], SEARCH_SPACE['gpu_layers']['high'])
 
 
     # Build llama-bench command (can edit to add more flags)
     cmd = [
-        llama_bench_path,        # path to your llama-bench binary
-        "-t", str(threads),
-        "--batch-size",  str(batch),   # (-b flag) (defaut 2024)
-        "--ubatch-size", str(u_batch), # (-ub flag) (defaut 512) 
-        "-ngl", str(gpu_layers),    # (-ngl or --n-gpu-layers flag)
-        "--flash-attn", str(flash), # enables Flash Attention, a performance optimization during inference. 
-        "--model", model_path,      # <--- change this or parametrize
-        "-n", "35",             # tokens to generate (larger value improve final statistics, i.e. lower std intok/s)
-        "-r", "2",              # number of benchmark runs for each configuration; mean value and std calculated from it 
-        "-o", "csv"             # save temporary .csv file with llama-bench outputs
+        llama_bench_path, # path to your llama-bench binary
+        "-t"            , str(threads),
+        "--batch-size"  , str(batch),      # (-b flag) (defaut 2024)
+        "--ubatch-size" , str(u_batch),    # (-ub flag) (defaut 512) 
+        "-ngl"          , str(gpu_layers), # (-ngl or --n-gpu-layers flag)
+        "--flash-attn"  , str(flash),      # enables Flash Attention, a performance optimization during inference. 
+        "--model"       , model_path,      # <--- change this or parametrize
+        "-r"            , "2",             # number of benchmark runs for each configuration; mean value and std calculated from it 
+        "-o"            , "csv"            # save temporary .csv file with llama-bench outputs
     ]
     # note: memory mapping is now set by defaut. Instead, need to add --no-map flag. 
 
+    # Add task-specific flags
+    if args.metric in ("tg", "mean"):
+        cmd += ["-n", "40"]  # tokens to generate (larger value improve final statistics, i.e. lower std in tok/s)
+    if args.metric in ("pp", "mean"):
+        cmd += ["-p", "40"]  # tokens to process (larger value improve final statistics, i.e. lower std in tok/s)
+
     try:
-        tokens_per_sec = run_llama_bench_with_csv(cmd)
+        tokens_per_sec = run_llama_bench_with_csv(cmd, arg.metric)
         return tokens_per_sec    
     except Exception as e:
         print(f"Error: {e}")
@@ -122,6 +139,8 @@ if __name__ == "__main__":
     parser.add_argument("--trials", type=int, default=35, help="Number of Optuna trials: n searches in parameter space")
     parser.add_argument("--model", type=str, help="Path to model (overrides env var)")
     parser.add_argument("--llama-bin", type=str, help="Path to llama.cpp build/bin folder (overrides env var)")
+    parser.add_argument("--metric", type=str, default="tg", choices=["tg", "pp", "mean"],
+    help="Which throughput metric to optimize: 'tg' (token generation, default), 'pp' (prompt processing), or 'mean' (average of both)")
     args = parser.parse_args()
 
     # Set paths based on CLI flags or env vars
@@ -145,6 +164,8 @@ if __name__ == "__main__":
 
     # estimate maximum number of layers before run 
     SEARCH_SPACE['gpu_layers']['high'] = estimate_max_ngl()
+    print(f"Setting maximum -ngl to {SEARCH_SPACE['gpu_layers']['high']}")
+
     run_optimization(n_trials=args.trials)
 
 
